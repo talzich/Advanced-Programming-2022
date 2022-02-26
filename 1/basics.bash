a50dc396b7f39e5925031d9bf4ab92e383278e6e
Tal <talzhtc@gmail.com>
2022-02-26 11:09:52 +0200

    first commit

---
+++ 1/basics.bash
@@ -0,0 +1,21 @@
+ls -l > ls_out      # Redirect stdout to file
+ls -l >> ls_out     # Append the output to the same file
+
+# ========
+# Question: Redirect a different command to the same file, what happens?
+# Example: echo abc > ls_out
+# ========
+
+ls -l 2> ls_err     # Rediredt stderr to a specific file
+# Why would we do that?
+
+
+ls -l > ls_out 2>&1 # Redirect stdout to ls_out and stderr to wherever stdout is
+
+# ========
+# Question: will the following command to the same thing? 
+2>&1 ls-l > ls_out
+# Write a C program to find out
+# ========
+
+ls -l 2> /dev/null
---
+++ 1/print_perror.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("STDOUT\n");
+	perror("STDERR\n");
+	return 0;
+}
