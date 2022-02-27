c7d63ada990464dd4f01c5e13693bd6f8e0c7a48
Tal <talzhtc@gmail.com>
2022-02-27 11:17:31 +0200

    new file

---
+++ 1/file_internals.bash
@@ -0,0 +1,11 @@
+cat <file>      # you better already know this one
+
+nl <file>       # same as cat, but with line numbers
+
+more <file>     # make scroll available
+
+less <file>     # better more
+
+most <file>     # better less
+
+https://askubuntu.com/questions/1191862/what-is-the-difference-between-more-and-less-commands
