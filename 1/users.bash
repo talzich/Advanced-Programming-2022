ddbed37a8b3a298638442615b23be5f015c88d1d
Tal <talzhtc@gmail.com>
2022-02-27 12:04:03 +0200

    minor

--- 1/users.bash
+++ 1/users.bash
@@ -4,6 +4,6 @@ who     # all users that are currently connected
 
 last    # last users that connected to the system
 
-chown   # change file's owner
+chown   # change file's owner https://linuxize.com/post/linux-chown-command/
 
-chmod   # change file's permissions
+chmod   # change file's permissions https://www.howtogeek.com/437958/how-to-use-the-chmod-command-on-linux/
