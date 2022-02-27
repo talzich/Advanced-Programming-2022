6abeeb21c1ff78c738fd092a80f801e7ed8bbe43
Tal <talzhtc@gmail.com>
2022-02-27 11:08:36 +0200

    new file

---
+++ 1/like_find.bash
@@ -0,0 +1,11 @@
+which <command>     # find the command's program file
+
+# ========
+# Exercise: find ls's program file
+# ========
+
+whereis <command>   # find command's files
+
+locate <command>    # find command's binaries 
+
+# What is the basic difference between the three? use the man command
